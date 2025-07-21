8c6318522ab209dc09ca39c4bbbf90086d83c5e6
SREENU42 <sreenulam4@gmail.com>
2025-07-21 17:32:29 +0530

    Main

---
+++ Main.java
@@ -0,0 +1,32 @@
+class Calculator {
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int subtract(int a, int b) {
+        return a - b;
+    }
+
+    int division(int a, int b) {
+        return a / b;
+    }
+
+    String name() {
+        return "sreenu";
+    }
+
+    boolean isNameSreenu() {
+        return name().equals("sreenu");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator c = new Calculator();
+        System.out.println("Addition: " + c.add(10, 5));
+        System.out.println("Subtraction: " + c.subtract(10, 5));
+        System.out.println("Division: " + c.division(10, 5));
+        System.out.println("Name: " + c.name());
+        System.out.println("Is name 'sreenu'? " + c.isNameSreenu());
+    }
+}
